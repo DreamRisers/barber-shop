cf34847187f3447f196080dfbfeb0f8ce053824c
JuanMatiasGema12 <166863870+JuanMatiasGema12@users.noreply.github.com>
2025-01-22 19:33:17 -0300

    Adding entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BarberModule } from './barber/barber.module';
+import { ClientModule } from './client/client.module';
+import { ServicesModule } from './services/services.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BarberModule, ClientModule, ServicesModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
